f35146f960bd624df729dfa7f4e623965428a025
Kirichenko Nickita <n.kirichenko@klienti.ru>
2020-06-16 01:01:08 +0300

    remove db depended sql code

--- src/database/init.php
+++ src/database/init.php
@@ -6,6 +6,8 @@
 require_once __DIR__."/../vendor/autoload.php";
 use App\DbManager;
 use App\Models\Role;
+use App\Models\Status;
+use App\Models\User;
 use Symfony\Component\Dotenv\Dotenv;
 
 
@@ -15,16 +17,28 @@ $dotenv->load(__DIR__.'/../.env');
 $roleRepository = DbManager::getRepository(Role::class);
 $roles = $roleRepository->find(1);
 if (empty($roles)) {
-    $sql = "insert roles(`value`) values ('admin');
-            insert statuses(`value`) values('new');
-            insert statuses(`value`) values('done');
-            insert users(`firstname`, `lastname`, `email`, `login`, `role_id`, `password`) values('admin', 'admin', 'admin@admin.com', 'admin', 1, '$2y$10$6AwuvnwgHqMdHWelHN5chucw0OlWsD6Dub99jJXv8n95PMgpZiYo.');";
-
     $manager = DbManager::createManager();
-    $statement = $manager->getConnection()->prepare($sql);
-    $statement->execute();
+    $role = new Role();
+    $role->setValue('admin');
+    $manager->persist($role);    
 
-    $statement->closeCursor();
+    $statusNew = new Status();
+    $statusNew->setValue('new');
+    $manager->persist($statusNew);
+
+    $statusDone = new Status();
+    $statusDone->setValue('done');
+    $manager->persist($statusDone);
+
+    $user = new User();
+    $user->setFirstname('admin');
+    $user->setLastname('admin');
+    $user->setEmail('admin@admin.admin');
+    $user->setLogin('admin');
+    $user->setPassword('123');
+    $manager->persist($user);
+    
+    $manager->flush();
 }
 
 
