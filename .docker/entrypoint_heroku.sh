23968c77c0d92c7e0d236e2654c1598bbc10c2f8
Kirichenko Nickita <n.kirichenko@klienti.ru>
2020-06-16 03:58:14 +0300

    fix

--- .docker/entrypoint_heroku.sh
+++ .docker/entrypoint_heroku.sh
@@ -34,4 +34,4 @@ sed -i -E "s/^user .*/user $(whoami);/" /etc/nginx/nginx.conf
 touch /etc/nginx/conf.d/default.template && \
     envsubst '\$PORT' < /etc/nginx/conf.d/default.conf > /etc/nginx/conf.d/default.template && \
     mv /etc/nginx/conf.d/default.template /etc/nginx/conf.d/default.conf && \
-    nginx -g 'daemon off;'
+    nginx
