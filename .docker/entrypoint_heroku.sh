504989f99c9449e5feeaca83b91d2212e6eeada0
Kirichenko Nickita <n.kirichenko@klienti.ru>
2020-06-16 04:40:38 +0300

    fix

--- .docker/entrypoint_heroku.sh
+++ .docker/entrypoint_heroku.sh
@@ -23,14 +23,6 @@ echo Give permissions to www-data to mounted volume directories
 # "/var/tmp/nginx" owned by "nginx" user is unusable on heroku dyno so re-create on runtime
 mkdir /var/tmp/nginx
 
-# make php-fpm be able to listen request from nginx (current user is nginx executor)
-sed -i -E "s/^;listen.owner = .*/listen.owner = $(whoami)/" /usr/local/etc/php-fpm.d/www.conf
-
-# make current user the executor of nginx and php-fpm expressly for local environment
-sed -i -E "s/^user = .*/user = $(whoami)/" /usr/local/etc/php-fpm.d/www.conf
-sed -i -E "s/^group = (.*)/;group = \1/" /usr/local/etc/php-fpm.d/www.conf
-sed -i -E "s/^user .*/user $(whoami);/" /etc/nginx/nginx.conf
-
 touch /etc/nginx/conf.d/default.template && \
     envsubst '\$PORT' < /etc/nginx/conf.d/default.conf > /etc/nginx/conf.d/default.template && \
     mv /etc/nginx/conf.d/default.template /etc/nginx/conf.d/default.conf && \
